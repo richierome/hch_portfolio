1128ffe6c97e43c51fccbd330e650279eeda1827
Rick Romero <rickromero@Ricks-MBP.fios-router.home>
2022-12-22 16:25:26 -0800

    updated

--- src/components/HeroAbout.js
+++ src/components/HeroAbout.js
@@ -10,7 +10,8 @@ function HeroAbout(){
     return (
         <div className='hero-container'>
     
-            <img className='razn-hell' src="/images/razn_hell_full.jpeg" alt='devil'/>
+            {/* <img className='razn-hell' src="/images/razn_hell_full.jpeg" alt='devil'/> */}
+            <img className='wolf-image' src="/images/wolf.png" alt='devil'/>
             
          </div>
     )
