0a323ab36a1eb4e0539a3f9a3c35c45b0001c69d
rick romero <rickromero99@gmail.com>
2026-05-30 17:05:05 -0700

    fixed stabilize app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,3 @@
-// import React, {useState, useEffect}from 'react';
-// // import { Button } from './Button';
-// import {Link} from 'react-router-dom';
-// import './Navbar.css'
-// import Bat from './bat/Bat';
-// import Bat2 from './bat/Bat2';
-
-
-// function Navbar(){
-//     const[click, setClick]= useState( false);
-//     const[button, setButton]= useState(true);
-
-//     const handleClick = () => setClick(!click);
-//     const closeMobileMenu = () => setClick(false);
-
-//     const showButton = () => {
-//         if(window.innerWidth <= 960) {
-//             setButton(false);
-//         } else {
-//             setButton(true);
-//         }
-//     };
-
-//     useEffect(() => {
-//         setButton();
-//     }, []);
-
-//     window.addEventListener('resize', showButton)
-
-//     return(
-//         <>
-//         <nav className='navbar'>
-//             <div className='navbar-container'>
-//                 <Link to ='/' className='navbar-logo' onClick={closeMobileMenu}>
-//                 </Link>
-//                 <div className='menu-icon' onClick={handleClick}>
-//                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-//                 </div>
-//                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-//                     <li className='nav-item'>
-//                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-//                     Home 
-//                     </Link>
-//                     </li>
-                    
-//                     <li className='nav-item'>
-//                     <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
-//                     About
-//                     </Link>
-//                     </li>
-
-//                     <li className='nav-item'>
-//                     <Link to='/Contact' className='nav-links' onClick={closeMobileMenu}>
-//                     Contact
-//                     </Link>
-//                     </li>
-                    
-//                     {/* <li>
-//                     <Link to='/sign-up'className='nav-links-mobile'onClick={closeMobileMenu}>
-//                         Sign Up
-//                     </Link>
-//                     </li> */}
-//                 </ul>
-//                 {/* {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} */}
-//             </div>
-//         </nav>
-//      </>
-//     );
-// }
-
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
@@ -95,7 +24,7 @@ function Navbar() {
   };
 
   useEffect(() => {
-    setButton();
+    showButton();
   }, []);
 
   useEffect(() => {
@@ -127,20 +56,14 @@ function Navbar() {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link
-                to='/About'
-                className='nav-links'
-                onClick={() => {
-                  closeMobileMenu();
-                }}
-              >
-                About
-              </Link>
+              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+               About
+            </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/Contact' className='nav-links' onClick={closeMobileMenu}>
-                Contact
-              </Link>
+             <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+            Contact
+            </Link>
             </li>
 
           </ul>
